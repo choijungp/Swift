f782845d86e36a9267c95052d39bb2f7c5e19353
choijungp <37467592+choijungp@users.noreply.github.com>
2022-06-29 15:52:42 +0900

    Add files via upload

---
+++ SwiftUI/Ch.1/Landmarks/Landmarks/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by 최정인 on 2022/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(ModelData())
+    }
+}
