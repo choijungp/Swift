23e7792f26a60c8ec98ad19ae81109ef605e7f90
choijungp <37467592+choijungp@users.noreply.github.com>
2022-06-30 21:27:35 +0900

    Add files via upload

---
+++ SwiftUI/Ch.3/Landmarks/Landmarks/Model/ModelData.swift
@@ -0,0 +1,50 @@
+//
+//  ModelData.swift
+//  Landmarks
+//
+//  Created by 최정인 on 2022/06/24.
+//
+
+import Foundation
+import Combine
+
+final class ModelData: ObservableObject {
+    @Published var landmarks: [Landmark] = load("landmarkData.json")
+    var hikes: [Hike] = load("hikeData.json")
+    @Published var profile = Profile.default
+    
+    var features: [Landmark] {
+        landmarks.filter { $0.isFeatured }
+    }
+    
+    var categories: [String: [Landmark]] {
+        Dictionary(
+            grouping: landmarks,
+            by: { $0.category.rawValue }
+        )
+    }
+}
+
+var landmarks: [Landmark] = load("landmarkData.json")
+
+func load<T: Decodable>(_ filename: String) -> T {
+    let data: Data
+    
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+    else {
+        fatalError("Couldn't find \(filename) in main bundle.")
+    }
+    
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
+    }
+    
+    do {
+        let decorder = JSONDecoder()
+        return try decorder.decode(T.self, from:data)
+    } catch {
+        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+    }
+}
